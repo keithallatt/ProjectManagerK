1e78c4c12fcfd4278e370aa48fe6126cdc0ee00f
Keith Allatt <keith.allatt@gmail.com>
2022-07-07 21:09:54 -0400

    "Sample Commit Message"

--- cmd_line_file.py
+++ cmd_line_file.py
@@ -54,6 +54,7 @@ def git_func(location, function, flags=None, *args):
     if args:
         output_lst += args
 
+    print(output_lst)
     cwd = os.getcwd()
     os.chdir(location)
     res = subprocess.check_output(output_lst)
